2781d2c4929d8b40f8f9f6c62842b367606a6fe4
Fabian Fabro <fabrofabian@gmail.com>
2019-02-20 18:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function theBeatlesPlay(musicians, instruments){
-    var counter = 0;
-    var newArray;
+    var newArray = [];
     for(var m = 0, i = 0; m < musicians.length && i < instruments.length; m++, i++){
       newArray.push(`${musicians[m]} plays ${instruments[i]}. `);
     }
